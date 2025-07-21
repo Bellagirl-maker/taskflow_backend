4daf0078f21cc0fbc3b7ca31512cbe695ae1da75
Isabella <isabellaotoo25@gmail.com>
2025-07-21 14:42:13 +0000

    Create task controller logic

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,2 +1,30 @@
 class Api::TasksController < ApplicationController
+    def create
+        task = Task.new(task_params)
+        if task.save
+            render json: task, status: :created
+        else
+            render json: task.errors, status: :unprocessable_entity
+        end
+    end
+    def update
+        task = task.find(params[:id])
+        if task.update(task_params)
+            render json: task
+        else
+            render json: task.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        task =  Task.find(params[:id])
+        task.destroy
+        head :no_content
+    end
+
+    private
+    def task_params
+        params.require(:task).permit(:title, :completed, :list_id)
+    end
+end
 end
